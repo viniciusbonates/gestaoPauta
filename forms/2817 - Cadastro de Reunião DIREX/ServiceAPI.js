f41c9c96e98fd8d9b857584a3d45cd9f6b21c51e
viniciubonates <vinicius.lira0.1.3@gmail.com>
2022-08-24 13:31:01 -0400

    orderMethods

--- forms/2817 - Cadastro de Reunião DIREX/ServiceAPI.js
+++ forms/2817 - Cadastro de Reunião DIREX/ServiceAPI.js
@@ -2,19 +2,18 @@ function resMethods(){
     res = {}
     return res
 }
-function order(){
+function orderMethods(){
+    this.Nsolicitacao        = 24920; 
+    this.host                = "http://10.4.4.52:8080";   
     resMethods();
-    //requestsGET();
-    movePOST();
 }
-function movePOST (){
-    var Nsolicitacao        = 24920;
-    var host                = "http://10.4.4.52:8080";     
-    requestsGET(Nsolicitacao);
+orderMethods.prototype.movePOST = function () {
+    var Nsolicitacao        = this.Nsolicitacao; 
+    var host                = this.host;     
+    this.requestsGET(this.Nsolicitacao, this.host);
     var interval = setInterval(pst, 100)
     function pst(){
-        var request             = window.res 
-        console.log(request)
+        var request = window.res 
         if(request.items.length != undefined){
             var movementSequence    = request.items.length;
             var targetState         = request.items[request.items.length - 1].state.sequence;
@@ -37,19 +36,14 @@ function movePOST (){
         }
     }
 }
-function requestsGET(NumSolicit) {
-    var Nsolicitacao    = 24920; 
-    var host            = "http://10.4.4.52:8080";
-    if(NumSolicit != '' && NumSolicit != undefined){
-        Nsolicitacao = NumSolicit
-    }
+orderMethods.prototype.requestsGET = function (NumSolicit, host) {
     $.ajax({
         method: "GET",
-        url: host+"/process-management/api/v2/requests/"+Nsolicitacao+"/activities?page=1&pageSize=1000",
+        url: host+"/process-management/api/v2/requests/"+NumSolicit+"/activities?page=1&pageSize=1000",
         contentType: "application/json"
     }).done(function (response) { 
         window.res = response;
     })
 }
-
-window.addEventListener('load', order)
+function orderMethodsInit() { orderMethodsMi = new orderMethods(); }
+window.addEventListener('load', orderMethodsInit)
